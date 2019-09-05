4c2326cf3d8782374ea356adf1f75786aff27b7c
Administrator <Administrator@Admin-PC>
2019-09-06 00:25:47 +0530

    update

--- googleSearchPage/src/test/java/google/qa/Testcases/SearchPageTest.java
+++ googleSearchPage/src/test/java/google/qa/Testcases/SearchPageTest.java
@@ -1,18 +1,37 @@
 package google.qa.Testcases;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
+
+
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+
 import google.qa.base.TestBase;
 import google.qa.searchpage.Searchpage;
 
-public class SearchPageTest extends TestBase {
-	Searchpage searchpage;
 
+
+public class SearchPageTest extends TestBase {
+	
+	Searchpage searchpage;
+	ExtentHtmlReporter reporter = new ExtentHtmlReporter("./Reporter/Googlereuslt.html");
+	ExtentReports extent= new ExtentReports();
+
+	
+
+	
 	public SearchPageTest() {
 		super();
 	}
@@ -24,21 +43,37 @@ public class SearchPageTest extends TestBase {
 	}
 
 	@Test(priority = 1)
-	public void loginPageTitleTest() {
+	
+		public void GooglepageTitleTest() {
+		extent.attachReporter(reporter);
 		String title = searchpage.validategooglePageTitle();
 		Assert.assertEquals(title, "Google");
+		
+		ExtentTest logger=extent.createTest("TitleTest");
+	    logger.log(Status.INFO, "Google ");
+	    logger.log(Status.PASS, "Google title verified");
+	    extent.flush();
+		
 	}
 
 	@Test(priority = 2)
-	public void GoogleLogoImageTest() {
+	public void GoogleLogoImageTest() throws Exception {
 		boolean flag = searchpage.validategoolgleImage();
 		Assert.assertTrue(flag);
+		ExtentTest logger=extent.createTest("GoogleLogoImageTest");
+	  	logger.log(Status.FAIL, "Google image verified");
+	    logger.fail("details", MediaEntityBuilder.createScreenCaptureFromPath("/screenshots/screenshot.png").build());
+	    extent.flush();
 	}
 
 	@Test(priority = 3)
 	public void GooglesearchtextTest() {
 		boolean flag = searchpage.validategoolgeSearchEdit();
 		Assert.assertTrue(flag);
+		ExtentTest logger=extent.createTest("GooglesearchtextTest");
+	    logger.log(Status.INFO, "Search text box ");
+	    logger.log(Status.PASS, "Google Search textbox verified");
+	    extent.flush();
 	}
 
 	@Test(priority = 4)
@@ -54,31 +89,45 @@ public class SearchPageTest extends TestBase {
 	@Test(priority = 5)
 	public void Googlesearchwithspecialchar() {
 		searchpage.Googlesearchwithsplchar("!@#$% - did not match any documents");
-
+		ExtentTest logger=extent.createTest("Googlesearchwithspecialchar");
+	    logger.log(Status.INFO, "Search with special char ");
+	    logger.log(Status.PASS, "Google Searchwith special char verified");
+	    extent.flush();
 	}
 
 	@Test(priority = 6)
 	public void Googlebtn() {
 		searchpage.validategooglesearchbtn();
 		searchpage.validateFeelluckybtn();
+		ExtentTest logger=extent.createTest("validategooglesearchbtn");
+	    logger.log(Status.INFO, "validate google search btn ");
+	    logger.log(Status.PASS, "Google Searchbutton verified");
+	    extent.flush();
 	}
 
 	@Test(priority = 7)
 	public void GoogleLanguagelnk() {
 		searchpage.validdategooglelnglink();
+		ExtentTest logger=extent.createTest("validdategooglelnglink");
+	    logger.log(Status.INFO, "google language link ");
+	    logger.log(Status.PASS, "Google language link verified");
+	    extent.flush();
 
 	}
 
 	@Test(priority = 8)
 	public void Googleautosearch() {
 		searchpage.autosearch();
-
+		ExtentTest logger=extent.createTest("Googleautosearch");
+	    logger.log(Status.INFO, "google Auto sugeestion verification ");
+	    logger.log(Status.PASS, "Google  Auto sugeestion verification verified");
+	    extent.flush();
 	}
 	
-	@Test(priority = 0)
+	/*@Test(priority = 0)
 	public void Googleaapps() {
 	searchpage.Validategoogleapps();
-	}
+	}*/
 	
 
 	@AfterMethod
